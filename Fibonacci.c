7350af86528885a0fffb45a976db8438dace9597
Clovis Monteiro da Silva Filho <34289102+ClovisMonteiro@users.noreply.github.com>
2018-05-02 22:08:41 -0300

    Add files via upload

---
+++ Fibonacci.c
@@ -0,0 +1,17 @@
+#include<istdio.h>
+
+
+
+int main(){
+	int a[14] = {0,1};
+	int i = 0;	
+
+	while(i < 12){ 
+		a[i+2] += a[i] + a[i+1]; 
+		printf("%i + %i = %i\n", a[i+1], a[i], a[i+2]); 
+		i++;
+	}
+
+    return 0;
+}
+
